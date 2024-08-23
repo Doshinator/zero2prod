5a0ae3353436ff035f0ce29cbd5eb04a6971b5ed
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-08-23 19:22:47 -0400

    added pgconnection to app for db connectoin

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,19 @@
-use actix_web::{dev::Server, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::{greeting, health_check, hello_world, name, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .service(hello_world)
             .service(greeting)
             .service(name)
             .service(health_check)
             .service(subscribe)
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
