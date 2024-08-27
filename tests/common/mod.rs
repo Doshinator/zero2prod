3bcf6ce8bbf0d3ce13949af3d69fe8d9c6d08d90
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-08-26 22:02:19 -0400

    configuration change to allow different db spin ups for each repetitive test that inserts same name & email in db and checks. because of uniqueness constraint, we chagne config to spin up db under different names

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,9 +4,10 @@
 // - if port 8000 is being used by another program on our machine (e.g. our own application!), tests will fail;
 // so this test will fail!!
 
-use zero2prod::configuration::get_configuration;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use std::net::TcpListener;
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -17,14 +18,14 @@ pub async fn spawn_app() -> TestApp {
     // port 0 to be able to randomize port assignemnt
     let addrs: &str = "127.0.0.1:0";
     let listener = TcpListener::bind(addrs).expect("failed to bind address");
-
-    let port = listener.local_addr().unwrap().port();
+        let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to bind address");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database)
+        .await;
 
     let server = zero2prod::startup::run(listener, connection_pool.clone())
         .expect("Failed to bind address");
@@ -34,4 +35,27 @@ pub async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name)
+        .as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres."); 
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
