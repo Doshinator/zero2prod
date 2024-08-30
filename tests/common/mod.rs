9dfc1f2a13b9ec848bec49fec4e5e66d60cd0aaa
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-08-30 00:58:33 -0400

    minor cleanup to allow dead code

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -13,6 +13,7 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+#[allow(dead_code)]
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
