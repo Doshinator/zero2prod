96e667c620c4062dedcc5a37c61117c07a93e183
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-08-30 18:37:11 -0400

    fmt

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,9 +1,7 @@
 //! src/routes/health_check.rs
 use actix_web::{get, HttpResponse, Responder};
 
-#[tracing::instrument(
-    name = "Health Check"
-)]
+#[tracing::instrument(name = "Health Check")]
 #[get("/health_check")]
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
