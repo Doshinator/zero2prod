be06590e43c6c929769f71977a15d00ac548c9ef
Doshi <doshi@Doshis-MacBook-Pro.local>
2024-09-06 16:47:56 -0400

    fmt and linter warning fix

--- src/configuration.rs
+++ src/configuration.rs
@@ -49,7 +49,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Init config setup
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-    
+
     // Detect the running environment.
     // Default to `local` if unspecified.
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
@@ -57,7 +57,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
     let environment_filename = format!("{}.yaml", environment.as_str());
-    
+
     let settings = config::Config::builder()
         .add_source(config::File::from(
             configuration_directory.join("base.yaml"),
@@ -78,7 +78,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
 pub enum Environment {
     Local,
-    Production
+    Production,
 }
 
 impl Environment {
@@ -92,14 +92,14 @@ impl Environment {
 
 impl TryFrom<String> for Environment {
     type Error = String;
-    fn try_from(s: String) -> Result<Self, Self::Error> { 
+    fn try_from(s: String) -> Result<Self, Self::Error> {
         match s.to_lowercase().as_str() {
-        "local" => Ok(Self::Local),
-        "production" => Ok(Self::Production),
-        other => Err(format!(
-            "{} is not a supported environment. Use either `local` or `production`.",
-            other
-        )),
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
         }
     }
 }
